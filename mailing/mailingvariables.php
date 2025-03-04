089cce6e84d1985cdc60ed19b4e1d5b58b0835a4
tonwayer <tonwayer@gmail.com>
2025-03-04 04:03:27 -0800

    fixed contactme

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
+    $mail_sender_email = "ijonathanwillis@gmail.com"; //sender
+    $mail_sender_password = "mohj jiis fmck exxr"; //sender
     $mail_sender_name = "Website Form";
 ?>
